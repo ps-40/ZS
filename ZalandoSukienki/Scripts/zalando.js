6b25bcd56b402dff6372cc52b5cdae9998b6cf9a
ps-40 <paulina.swistacz@wp.pl>
2016-10-31 10:24:26 +0100

    added accordion

---
+++ ZalandoSukienki/Scripts/zalando.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('.dresses').after('to jest tekst z Scripts/zalando.js');
+});
+$(document).ready(function () {
+    $(".dresses > li > div").click(function () {
+
+        /*if (false == $(this).next().is(':visible')) {
+            $('.dresses ul').slideUp(300);
+        }*/
+        $(this).next().slideToggle(300);
+    });
+
+    $('.dresses ul:eq(0)').show();
+});
